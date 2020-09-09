4060a673711b53367a0a466db8405693df3735f7
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-09-09 18:40:27 -0300

    Feito Tela de Login, corrigido erros e já adicionado o Router e Links

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  const regexEmail = /\S+@\S+\.\S+/i;
+  const validateEmail = new RegExp(regexEmail);
+  const regExTest = validateEmail.test(email);
+
+  const Verify = () => !(regExTest && pass.length > 6);
+
+  const setToken = () => {
+    const storeEmail = { email: `${email}` };
+    localStorage.setItem('user', JSON.stringify(storeEmail));
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <input
+        data-testid="email-input"
+        type="email"
+        placeholder="Email"
+        onChange={(event) => {
+          setEmail(event.target.value);
+        }}
+      />
+      <input
+        data-testid="password-input"
+        type="password"
+        placeholder="Senha"
+        onChange={(event) => {
+          setPass(event.target.value);
+        }}
+      />
+      <Link to="/comidas" onClick={() => setToken()}>
+        <button
+          type="button"
+          data-testid="login-submit-btn"
+          disabled={Verify()}
+        >
+          Entrar
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default Login;
