5c7a3e2a0061ef898d15849e86c84565669311c3
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-09-10 14:10:25 -0300

    codeclimate

--- src/components/Comidas/index.jsx
+++ src/components/Comidas/index.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import Header from '../Header';
 
-const Comidas = () => {
-  return (
-    <Header />
-  );
-};
+const Comidas = () =>  <Header />;
 
 export default Comidas;
