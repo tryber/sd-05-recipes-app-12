d0136c4f555b1792801bc8571538cf1481d954e8
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-09-14 20:32:39 -0300

    mainpage

---
+++ src/pages/MainPage/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../../components/Header';
+import SearchBar from '../../components/SearchBar';
+import RecipeCard from '../../components/SearchBar/RecipeCard';
+
+const MainPage = () => {
+  return (
+    <div>
+      <Header />
+      <SearchBar />
+      <RecipeCard />
+    </div>
+  );
+};
+
+export default MainPage;
