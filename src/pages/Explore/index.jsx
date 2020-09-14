deb190360b0c83196d898fb46dfbc91a3f28f5f9
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-09-14 20:20:17 -0300

    feito pagina de Explore com dois buttons

--- src/pages/Explore/index.jsx
+++ src/pages/Explore/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import Header from '../../components/Header';
+import { Link } from 'react-router-dom';
 
 export const Explorar = () => (
   <div>
     <Header />
+    <Link to="/explorar/comidas">
+      <button data-testid="explore-food">Explorar Comidas</button>
+    </Link>
+    <Link to="/explorar/bebidas">
+      <button data-testid="explore-drinks">Explorar Bebidas</button>
+    </Link>
   </div>
 );
 
