bf61045080f177083ab68c7e2411418fcd53af8d
hleoc <informig@gmail.com>
2020-09-17 12:30:21 -0300

    Telas detalhes Bebida e detalhes Bebida progress

--- src/utils/utilities.jsx
+++ src/utils/utilities.jsx
@@ -3,22 +3,22 @@ export async function verify(pathName, id, setDataDetail) {
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
     const data = await response.json();
     setDataDetail(data.meals[0]);
-  }
-  if (pathName === `/bebidas/${id}`) {
+  } else if (pathName === `/bebidas/${id}`) {
     const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
     const data = await response.json();
-    setDataDetail(data.meals[0]);
+    setDataDetail(data.drinks[0]);
   }
 }
 
-export async function recommended(pathName, setMeal) {
+export async function recommended(pathName, setMeal, setDdrink) {
   if (pathName.match(/comidas/)) {
     const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
     const data = await response.json();
     setMeal(data.drinks);
-  } else if (pathName.match(/bebidas/)) {
+  }
+  if (pathName.match(/bebidas/)) {
     const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
     const data = await response.json();
-    setMeal(data.drinks);
+    setDdrink(data.meals);
   }
 }
