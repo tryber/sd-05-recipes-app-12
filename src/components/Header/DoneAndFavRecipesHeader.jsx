7fc12b60aed606472074853e223d227f89176904
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-09-18 17:16:00 -0300

    codeclimate fix 2

--- src/components/Header/DoneAndFavRecipesHeader.jsx
+++ src/components/Header/DoneAndFavRecipesHeader.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-const DoneAndFavRecipesHeader = () => {
-  return(
- <div>
-      <button data-testid="filter-by-all-btn">All</button>
-      <button data-testid="filter-by-food-btn">Meals</button>
-      <button data-testid="filter-by-drink-btn">Drink</button>
-</div>
-  )
-}
+const DoneAndFavRecipesHeader = () => (
+  <div>
+    <button data-testid="filter-by-all-btn">All</button>
+    <button data-testid="filter-by-food-btn">Meals</button>
+    <button data-testid="filter-by-drink-btn">Drink</button>
+  </div>
+)
 
 export default DoneAndFavRecipesHeader;
