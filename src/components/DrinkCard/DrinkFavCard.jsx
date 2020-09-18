c571a45935fdf0fc63bf25387c19e2131ee8be15
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-09-18 16:39:34 -0300

    adicionando card drink favorite

---
+++ src/components/DrinkCard/DrinkFavCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const DrinkFavCard = () => { 
+  return (
+    
+  <>data-testid={`${index}-horizontal-top-text`}</>
+  <>data-testid={`${index}-horizontal-name`}</>
+  <> data-testid={`${index}-horizontal-done-date`} </>
+  <> data-testid={`${index}-${tagName}-horizontal-tag`} </>
+  <img data-testid={`${index}-horizontal-share-btn`} src="" />
+);
+}
+
+export default DrinkFavCard;
