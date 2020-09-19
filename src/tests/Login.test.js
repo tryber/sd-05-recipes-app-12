a3a2cede571c3e6ae63e86f69e299b404ad56212
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-09-19 17:48:18 -0300

    refazendo testes

---
+++ src/tests/Login.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { fireEvent, cleanup } from '@testing-library/react';
+import renderWithRouter from '../__render__/renderWithRouter';
+
+import Login from '../pages/Login';
+
---
+++ src/tests/renderWithRouter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+
+const renderWithRouter = (component, initialRoute = ['/']) => {
+  const history = createMemoryHistory({ initialRoute });
+  return {
+    ...render(<Router history={history}>{component}</Router>),
+    history,
+  };
+};
+
+export default renderWithRouter;
+// Felipe passou essa função pra contornar erros de teste causados pelo context
